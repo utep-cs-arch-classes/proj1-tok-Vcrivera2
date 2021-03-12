60664bcc471d4570d13fba9c773eacce05a2f279
Vcrivera2 <vcrivera2@miners.utep.edu>
2021-03-12 12:18:00 -0700

    added the count words using previous funcs

--- src/gui.c
+++ src/gui.c
@@ -25,6 +25,7 @@ int main(){
 
 
 
+//will check for a space andreturn 1 for true if found
 int space_char(char c){
 
   if(c == ' ' || c == '\t' || c == '\n' && c != '\0'){
@@ -38,7 +39,7 @@ int space_char(char c){
 
 
 
-
+//will check for non space return 1 if found
 int non_space_char(char c){
   if( c != ' ' && c != '\t' && c != '\n' && c != '\0'){
     return 1;
@@ -50,19 +51,25 @@ int non_space_char(char c){
 
 char *word_start(char *str){ //to first non space character
 
+  //save address and get a check using non_space_char
   char *x = str;
   int check = non_space_char(*x);
 
+  //will continue if we keep reading a space or not at the end
   while(check == 0 && *x != '\0'){
     check = non_space_char(*x);
 
+    //if we find a space break
     if (check == 1){
       break;
     }
-    x++;
 
+    //if not increment the char string
+    x++;
   }
 
+  //at the end we check to see if empty if not return the pointer to the start
+  //of the next word
   if(*x == '\0'){
     return 0;
   }
@@ -75,19 +82,28 @@ char *word_start(char *str){ //to first non space character
 
 char *word_end(char *str){ //to for space character after end of word
 
+  //saving the pointer of the string and sending
   char *x = str;
   int check = space_char(*x);
 
+  //continue if we still have a non space word
+  //or we are not at the end of the string
   while(check == 1 && *x!= '\0'){
 
+    //save our chec
     check = space_char(*x);
+
+    //if we read a space break the loop
     if(check == 0){
       break;
     }
+    //if not continue down the string
     x++;
     
   }
 
+  //check to see if we have a empty string
+  //if not return the end pointer of the string
   if(*x=='\0'){
     return 0;
   }
@@ -96,9 +112,6 @@ char *word_end(char *str){ //to for space character after end of word
     return x;
   }
 
-
-  
-
 }
 
 
@@ -106,6 +119,25 @@ char *word_end(char *str){ //to for space character after end of word
 int count_words(char *str){
 
 
+  char *x = str;
+  int count =0;
+
+  while(*x != '\0'){
+
+    if(space_char(*x) == 0){
+      x = word_end(x);
+      count++;
+    }
+    if(space_char(*x) == 1){
+      x= word_start(x);
+    }
+    else{
+      x++;
+    }
+
+    return count;
+    
+  }
 
 }
 
