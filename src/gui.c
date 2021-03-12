1d3d101769811640cb3e2a3de923b46f91250a9a
Vcrivera2 <vcrivera2@miners.utep.edu>
2021-03-12 10:35:41 -0700

    added updates to gui.c and remove unnecesary file

--- src/gui.c
+++ src/gui.c
@@ -1,17 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
 #define LIMIT 100
-char arr[LIMIT];
+
 
 int main(){
 
-  int i = 0;
-  for(char input_char; (input_char = getchar())!= '\n' && i < LIMIT; i++){
+  char input[LIMIT];
+  char c;
+  int i;
 
-    arr[i] = input_char;
-    putchar(input_char);
+    printf(">");
+  
+    for(i = 0; (c = getchar()) != '\n' && i < LIMIT - 1; i++){
+      input[i] = c;
+    }
+
+    printf("%s\n", input);
+    input[i] = '\0';
+    
+  
+}
 
 
+
+int space_char(char c){
+
+  if(c == ' ' || c == '\t' || c == '\n' && c != '\0'){
+    return 1;
+    
   }
-  printf("\n");
+
+  return 0;
+  
+}
+
+
+
+
+int non_space_char(char c){
+  if( c != ' ' && c != '\t' && c != '\n' && c != '\0'){
+    return 1;
+  }
+
+  return 0;
+}
+
+
+char *word_start(char *str){ //to first non space character
+
 
 }
+
+
+
+char *word_end(char *str){ //to for space character after end of word
+
+
+}
+
+
+
+int count_words(char *str){
+
+
+
+}
+
