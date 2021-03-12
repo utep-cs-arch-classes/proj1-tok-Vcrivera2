a923fd29e56ea5c6f6e8563eb210de6779812dd9
Vcrivera2 <vcrivera2@miners.utep.edu>
2021-03-12 11:43:53 -0700

    added word start and word end usince space and non space func

--- src/gui.c
+++ src/gui.c
@@ -50,13 +50,54 @@ int non_space_char(char c){
 
 char *word_start(char *str){ //to first non space character
 
+  char *x = str;
+  int check = non_space_char(*x);
 
+  while(check == 0 && *x != '\0'){
+    check = non_space_char(*x);
+
+    if (check == 1){
+      break;
+    }
+    x++;
+
+  }
+
+  if(*x == '\0'){
+    return 0;
+  }
+  else{
+    return x;
+  }
 }
 
 
 
 char *word_end(char *str){ //to for space character after end of word
 
+  char *x = str;
+  int check = space_char(*x);
+
+  while(check == 1 && *x!= '\0'){
+
+    check = space_char(*x);
+    if(check == 0){
+      break;
+    }
+    x++;
+    
+  }
+
+  if(*x=='\0'){
+    return 0;
+  }
+  else{
+
+    return x;
+  }
+
+
+  
 
 }
 
